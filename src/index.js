df41a017b034689f0f23568d7e0c25d1fc047753
carlos_reyes <carlosarp2000@gmail.com>
2022-01-05 20:10:17 -0500

    modulo cors agregado

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const cors = require("cors");
 
-require('./database');
+require("./database");
 
+app.use(cors());
 app.use(express.json());
 
-app.use('/api' ,require('./routes/index'))
+app.use("/api", require("./routes/index"));
 
 app.listen(3000);
-console.log('server on port', 3000);
+console.log("server on port", 3000);
